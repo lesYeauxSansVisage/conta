000362ae4c3fc9f7f383ae1aec9403539965daec
Gen <genesishawkeye42@gmail.com>
2023-01-03 21:01:35 -0300

    created the debitos and creditos array

--- src/abstract/Conta.ts
+++ src/abstract/Conta.ts
@@ -1,5 +1,12 @@
-abstract class Conta {
+import { Credito } from "../constuctors/Crédito";
+import { Debito } from "../constuctors/Débito";
+
+export abstract class Conta {
   private _numero: string;
+
+  private _creditos: Credito[] = [];
+
+  private _debitos: Debito[] = [];
 
   constructor(numero: string) {
     this._numero = numero;
@@ -11,5 +18,21 @@ abstract class Conta {
 
   public set numero(value: string) {
     this._numero = value;
+  }
+
+  public depositar(valor: number) {
+    const data = new Date();
+    const novoCredito = new Credito(valor, data);
+    this._creditos.push(novoCredito);
+  }
+
+  public sacar(valor: number, limite: number) {
+    if (limite - valor < 0) {
+      throw new Error("Valor do saque não pode ser maior que o limite!");
+    }
+
+    const data = new Date();
+    const novoDebito = new Debito(valor, data);
+    this._debitos.push(novoDebito);
   }
 }
