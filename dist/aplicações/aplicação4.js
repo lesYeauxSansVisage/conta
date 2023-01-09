99e34d15440a031fbf6b9e3dcfd5950b2a132b11
Gen <genesishawkeye42@gmail.com>
2023-01-09 16:36:06 -0300

    added logs for the transferir method

--- dist/aplicações/aplicação4.js
+++ dist/aplicações/aplicação4.js
@@ -2,9 +2,11 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const Cliente_1 = require("../Cliente");
 const cliente1 = new Cliente_1.Cliente("12345678901", "João", "988887744", "corrente", "4044444", "Ladeira", "15", "Próximo a igreja", "São Paulo", "SP");
-cliente1.correntes[0].depositar(1000, 2023, 1, 5);
 const cliente2 = new Cliente_1.Cliente("12345678901", "William", "988887744", "poupança", "4044444", "Vila", "15", "Próximo a escola", "São Paulo", "SP");
+cliente1.correntes[0].depositar(1000, 2023, 1, 5);
 cliente2.poupancas[0].depositar(1000, 2023, 5, 1);
+console.log(`O saldo de ${cliente1.nome} é ${cliente1.correntes[0].calcularSaldo()}.`);
+console.log(`O saldo de ${cliente2.nome} é ${cliente2.poupancas[0].calcularSaldo()}.`);
 cliente1.correntes[0].transferir(cliente2.poupancas[0], 500, 2023, 1, 5);
-console.log("O saldo do cliente 1 é: " + cliente1.correntes[0].calcularSaldo() + "R$");
-console.log("O saldo do cliente 2 é: " + cliente2.poupancas[0].calcularSaldo() + "R$");
+console.log(`O saldo de ${cliente1.nome} é ${cliente1.correntes[0].calcularSaldo()}.`);
+console.log(`O saldo de ${cliente2.nome} é ${cliente2.poupancas[0].calcularSaldo()}.`);
