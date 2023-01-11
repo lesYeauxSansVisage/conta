bddf333d4937d2169bc152e1c60817ddc5a2b723
Gen <genesishawkeye42@gmail.com>
2023-01-11 10:17:35 -0300

    fixed constructor

--- dist/constuctors/Endereco.js
+++ dist/constuctors/Endereco.js
@@ -2,14 +2,13 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.Endereco = void 0;
 class Endereco {
-    constructor(cep, logradouro, numero, complemento, cidade, uf, cliente) {
+    constructor(cep, logradouro, numero, complemento, cidade, uf) {
         this._cep = cep;
         this._logradouro = logradouro;
         this._numero = numero;
         this._complemento = complemento;
         this._cidade = cidade;
         this._uf = uf;
-        this._cliente = cliente;
     }
     get cep() {
         return this._cep;
